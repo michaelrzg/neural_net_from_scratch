6a3ec9f18eca8cacbe783ca8e47e9faf87ff5f92
michaelrzg <michaelrizig@gmail.com>
2024-05-12 18:16:38 -0400

    renamed

---
+++ main.py
@@ -0,0 +1,45 @@
+# this file contains main file for the neural network
+# michael rizig
+# 5/12/24
+
+import numpy as np #for math
+from layers import dense
+from data_generation.generateSpiralData import generateData #from generateData file
+from activation_functions import rectifyLinear,softmax # from activation_functions folder
+from loss_functions.categoricalCrossEntropy import categoricalCrossEntropy
+
+#_main:
+
+#eulers number (e^x) used for softmax
+E = 2.71828182846
+
+#generate data
+np.random.seed(123)
+X,y = generateData(100,3)
+
+# define first layer & activation
+dense1 =dense.layer_dense(2,3)
+activation1 = rectifyLinear.Act_rectefiedLinear()
+
+#define second layer and activation
+dense2 = dense.layer_dense(3,3)
+activation2 = softmax.Act_softmax()
+
+#forward propogation
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+#print results of forward propogation
+firstpass= activation2.output
+
+#categorical cross entropy to calculate loss
+lossfunc = categoricalCrossEntropy()
+passloss = lossfunc.calculateLoss(firstpass,y)
+
+#get accuracy
+predictions = np.argmax(firstpass,axis=1)
+accuracy = np.mean(predictions==y)
+print(passloss)
+print(accuracy)
